58d9581e39e9cc97913a119cd7419b75dd408f93
Gajulavarthi Kalyan Rao <kalyanrao135@gmail.com>
2025-12-29 10:55:25 +0530

    Promise File Added

--- promise.js
+++ promise.js
@@ -2,10 +2,4 @@ const promise = new Promise((reslove,reject)=>{
     if(true) reslove("Promise Resloved");
     reject("Errors");
 });
-promise.then(()=>{
-    console.log(reslove);
-}).catch(()=>{
-    console.log("Error");
-});
-console.log(promise.reslove)
-console.log(promise.reject)
+
