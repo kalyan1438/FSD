d84904cfca5df94aeec1b1fbd42a04594924541e
Gajulavarthi Kalyan Rao <kalyanrao135@gmail.com>
2025-12-29 11:19:05 +0530

    Promise File Added

--- promise.js
+++ promise.js
@@ -2,6 +2,7 @@ function process(oId){
     return new Promise((reslove,reject)=>{
         const time = 500 + Math.random()*100 + 5000;
         setTimeout(() => {
+            if(5500<time) reject("Error");
             reslove({
                 oId,status:"Completed",ptime: Math.round(time)
             });
@@ -10,7 +11,10 @@ function process(oId){
 }
 const obatch = ["o1","o2","o3"];
 Promise.all(obatch.map(process)).then((res)=>{
+    try{
     console.log("All orders are complete");
     console.table(res);
-})
+    }
+    catch(e) {console.log(e);}
+}).catch((e)=>console.log(e));
 
