4d57dfa5e88173141c88794fe4ffe070ea020186
Gajulavarthi Kalyan Rao <kalyanrao135@gmail.com>
2025-12-31 12:05:17 +0530

    Added

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-setTimeout(()=>{
-    const div = document.querySelector("#div");
-    div.innerHTML="<h1>HELLO</h1>";
-},2000);
+const display=document.getElementById("dis");
+
+const getdata = async (id)=>{
+    try{
+    if(id<1 || id>10) throw Error("Id Not Found");
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const data = await res.json();
+    display.innerHTML=`<h1>User Name:${data.name}</h1><br><p>${data.email}</p>`;
+    }
+    catch(e){
+        alert("Please Enter Vaild Id",e);
+    }
+}
+function sample(){
+    const id = document.getElementById("num").value;
+    getdata(id);
+}
