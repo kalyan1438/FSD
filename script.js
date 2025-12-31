d9351a6aa884b9a37c07232069ca2bfb50dc676f
Gajulavarthi Kalyan Rao <kalyanrao135@gmail.com>
2025-12-31 12:13:33 +0530

    Sample

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const display=document.getElementById("dis");
 const getdata = async (id)=>{
     try{
     if(id<1 || id>10) throw Error("Id Not Found");
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/`);
     const data = await res.json();
     display.innerHTML=`<h1>User Name:${data.name}</h1><br><p>${data.email}</p>`;
     }
@@ -13,6 +13,5 @@ const getdata = async (id)=>{
     }
 }
 function sample(){
-    const id = document.getElementById("num").value;
-    getdata(id);
+    getdata();
 }
