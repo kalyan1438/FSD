22193eaec1b085f5fbeb4d3e5a186bf1042caee5
Gajulavarthi Kalyan Rao <kalyanrao135@gmail.com>
2026-01-03 21:00:49 +0530

    Sample

---
+++ script.js
@@ -0,0 +1,9 @@
+const getData = async()=>{
+    const res = await fetch("");
+    const data = await res.json();
+    const id = document.querySelector();
+      // vs
+    document.querySelector("").innerHTML = (await((await fetch("")).json()))
+}
+function greet(a,a) {return a*a;}
+console.log(greet(2,3));
