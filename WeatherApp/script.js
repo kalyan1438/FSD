df4909b45c6807042b141d8284f3e93a639841b1
Kalyan Rao <kalyanrao135@gmail.com>
2026-01-13 20:42:46 +0530

    dd

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -1,6 +1,6 @@
 const city = document.querySelector("#city").value.trim();
 const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=d560d3a9ac71a1e87062423c94c65417`;
-const sample = async ()=>{
+const seample = async ()=>{
     const res = await fetch(url)
     const data  = await res.json();
     const dis = document.querySelector("#dis")
